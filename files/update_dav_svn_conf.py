starofrainnight/docker-subversion

commit b85923de4ce276d3d494aef929dd77e860dcd7b9

Fixed repository/db folder be seem as a repository directory

files/update_dav_svn_conf.py
@@ -38,7 +38,8 @@ for root, dirs, files in os.walk(parent_prefix):
     for adir in dirs:
         adir_path = os.path.normpath(os.path.join(root, adir))
         format_path = os.path.join(adir_path, "format")
-        if os.path.exists(format_path):
+        svnserve_path = os.path.join(adir_path, "conf/svnserve.conf")
+        if not (os.path.exists(format_path) and os.path.exists(svnserve_path)):
             dont_visit_dirs.append(adir)
             continue
 
@@ -54,7 +55,8 @@ for root, dirs, files in os.walk(parent_prefix):
 
 content = "\n".join(dst_contents)
 
-print("Origin content size : %s, New content size : %s" % (len(origin_content), len(content)))
+print("Origin content size : %s, New content size : %s" %
+      (len(origin_content), len(content)))
 
 if origin_content != content:
     with open(dav_conf_file_path, "wb") as dst_file:

commit c4240032ecdeccfe50e5e4cd20f52f9ae8d20770

Fixed repository did not show up
Problem introduced by b85923de4ce276d3d494aef929dd77e860dcd7b9

files/update_dav_svn_conf.py
@@ -39,7 +39,11 @@ for root, dirs, files in os.walk(parent_prefix):
         adir_path = os.path.normpath(os.path.join(root, adir))
         format_path = os.path.join(adir_path, "format")
         svnserve_path = os.path.join(adir_path, "conf/svnserve.conf")
-        if not (os.path.exists(format_path) and os.path.exists(svnserve_path)):
+        # Don't visit repositories, otherwise the repositories lead apache2
+        # happen to this error:
+        #
+        #     The URI does not contain the name of a repository
+        if (os.path.exists(format_path) and os.path.exists(svnserve_path)):
             dont_visit_dirs.append(adir)
             continue
 