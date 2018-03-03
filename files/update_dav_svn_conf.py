4cc94fca91f53afe15983e4a79d1cfb01591ae8b
Hong-She Liang <starofrainnight@gmail.com>
2018-03-03 18:28:03 +0800

    Support webdav repository
    You just need to add an empty webdav.conf inside 'conf' directory

--- files/update_dav_svn_conf.py
+++ files/update_dav_svn_conf.py
@@ -1,7 +1,8 @@
 import os
 import os.path
 
-location_prefix = "/svn"
+standard_location_prefix = "/svn"
+webdav_location_prefix = "/webdav"
 parent_prefix = "/var/lib/svn"
 
 dav_conf_file_path = "/etc/apache2/mods-available/dav_svn.conf"
@@ -11,11 +12,26 @@ if os.path.exists(dav_conf_file_path):
         origin_content = src_file.read()
         src_file.close()
 
-template = """
+standard_template = """
 <Location %s>
 	DAV svn
 	SVNParentPath %s
+	AuthzSVNAccessFile /etc/apache2/dav_svn/authz
 
+	Satisfy any
+	Require valid-user
+	AuthType Basic
+	AuthName "Subversion"
+	AuthUserFile /etc/apache2/dav_svn/htpasswd
+</Location>
+"""
+
+webdav_template = """
+<Location %s>
+	DAV svn
+	SVNPath %s
+	SVNAutoversioning on
+	ModMimeUsePathInfo on
 	AuthzSVNAccessFile /etc/apache2/dav_svn/authz
 
 	Satisfy any
@@ -31,7 +47,8 @@ dst_contents = []
 # Append the first level directory if it's not the repository
 format_path = os.path.join(parent_prefix, "format")
 if not os.path.exists(format_path):
-    dst_contents.append(template % (location_prefix, parent_prefix))
+    dst_contents.append(
+        standard_template % (standard_location_prefix, parent_prefix))
 
 for root, dirs, files in os.walk(parent_prefix):
     dont_visit_dirs = []
@@ -39,19 +56,31 @@ for root, dirs, files in os.walk(parent_prefix):
         adir_path = os.path.normpath(os.path.join(root, adir))
         format_path = os.path.join(adir_path, "format")
         svnserve_path = os.path.join(adir_path, "conf/svnserve.conf")
+        webdav_conf_path = os.path.join(adir_path, "conf/webdav.conf")
+
+        suffix_dir = adir_path[len(parent_prefix) + 1:]
+        new_parent = os.path.join(parent_prefix, suffix_dir)
+
         # Don't visit repositories, otherwise the repositories lead apache2
         # happen to this error:
         #
         #     The URI does not contain the name of a repository
         if (os.path.exists(format_path) and os.path.exists(svnserve_path)):
             dont_visit_dirs.append(adir)
+
+            # Map webdav repository to "/webdav" prefix
+            if (os.path.exists(webdav_conf_path)
+                    and os.path.isfile(webdav_conf_path)):
+
+                new_location = os.path.join(
+                    webdav_location_prefix, suffix_dir)
+                content = webdav_template % (new_location, new_parent)
+                dst_contents.append(content)
+
             continue
 
-        suffix_dir = adir_path[len(parent_prefix) + 1:]
-        new_location = os.path.join(location_prefix, suffix_dir)
-        new_parent = os.path.join(parent_prefix, suffix_dir)
-
-        content = template % (new_location, new_parent)
+        new_location = os.path.join(standard_location_prefix, suffix_dir)
+        content = standard_template % (new_location, new_parent)
         dst_contents.append(content)
 
     for adir in dont_visit_dirs:
